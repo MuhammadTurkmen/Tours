b12a43957e98d347d1961e80d86c917bfd266ccd
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 15:00:29 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -4,6 +4,9 @@ const Tour = ({id, image, info, price, name}) => {
 
   return <article className='single-tour'>
     <img src={image} alt={name} />
+    <footer>
+      
+    </footer>
   </article>
 };
 
