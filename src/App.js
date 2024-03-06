e1806d338defc585606b0315d514779f52226852
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 20:47:12 +0430

    init

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
+  
+  const fetchTours = async () => {
+    
+  }
+  
+  
   if(loading) {
     return <main>
       <Loading />
