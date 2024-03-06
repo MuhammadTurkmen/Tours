59c1d7e60942e5ec91f9831eb707e3568b24ed0f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 11:51:02 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -9,7 +9,7 @@ const Tour = ({id, image, info, price, name}) => {
         <h4>{name}</h4>
         <h4 className='tour-price'>${price}</h4>
       </div>
-      <p>{readMore ? info : `${info.substring(0, 200)}`
+      <p>{readMore ? info : `${info.substring(0, 200)}...`
       
       }
       </p>
