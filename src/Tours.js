bc3629efe96abd8f6750f62a4a44c936265f410c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 21:10:14 +0430

    init

--- src/Tours.js
+++ src/Tours.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Tour from './Tour';
 const Tours = ({tours}) => {
   return <section>
-    <div className="title"></div>
+    <div className="title">
+      <h2>our tours</h2>
+    </div>
   </section>;
 };
 
