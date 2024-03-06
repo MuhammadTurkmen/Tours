ecb1ae69ca26b5cce7af0b629d7bfa3254323129
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 11:48:29 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -9,8 +9,9 @@ const Tour = ({id, image, info, price, name}) => {
         <h4>{name}</h4>
         <h4 className='tour-price'>${price}</h4>
       </div>
-      <p>
-        {info}
+      <p>{readMore ? info : 
+      info
+      }
       </p>
       <button className='delete-btn'>
         not interested
