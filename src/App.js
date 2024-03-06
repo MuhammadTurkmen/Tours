fd0e518de4a9ab32fccf586242bb579e953b5538
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 20:41:16 +0430

    init

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
   return <h2>Tours Project Setup</h2>
 }
 
