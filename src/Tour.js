27ed957de2fa4394faa5ef5ea2e17a7eea63a313
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 14:47:33 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
-  return <h2>tour component</h2>;
+
+  return <article>
+    
+  </article>
 };
 
 export default Tour;
