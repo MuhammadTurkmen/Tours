046dd2fbfb540afa30b2bcd47c2c704d3e50e16b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 14:47:57 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
 
-  return <article>
-    
+  return <article className='single-tour'>
+    <img src={image} alt="" />
   </article>
 };
 
