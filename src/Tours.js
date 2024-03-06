b6b6d4b6217456c4f342b359a1f483a228dc8b5b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 21:02:21 +0430

    init

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = ({tours}) => {
-  return <h2>tours component</h2>;
+  return <section>tours component</section>;
 };
 
 export default Tours;
