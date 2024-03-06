bee88350ce2a48118ac4a2784516e5f2c922c634
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 11:47:33 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
-  const [readMore, setReadMore] = useState()
+  const [readMore, setReadMore] = useState(false)
   return <article className='single-tour'>
     <img src={image} alt={name} />
     <footer>
