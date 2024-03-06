d92264694c884160019b952d05326eb007af5e3e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 11:44:43 +0430

    init

--- src/Tour.js
+++ src/Tour.js
@@ -9,6 +9,9 @@ const Tour = ({id, image, info, price, name}) => {
         <h4>{name}</h4>
         <h4 className='tour-price'>${price}</h4>
       </div>
+      <p>
+        {info}
+      </p>
     </footer>
   </article>
 };
