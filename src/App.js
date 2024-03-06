bacb4d473632262c42c2e8334436bc2576eddff2
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-06 12:09:01 +0430

    init

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     return <main>
       <div className="title">
         <h2>no tours left</h2>
-        <button></button>
+        <button onClick={() => fetchTours()}></button>
       </div>
     </main>
   }
